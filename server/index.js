5e3c69c047dc49f0e4cd838c4db61e48a3292ba1
yefringeovany <yperezm18@miumg.edu.gt>
2024-05-31 12:37:29 -0600

    Intento deploy

--- server/index.js
+++ server/index.js
@@ -9,19 +9,32 @@ const app = express();
 // Conexión a la base de datos
 MongoDBConnection();
 
-// middleware
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-	res.locals.path = req.path;
-	next();
-});
 
+// Configuración de CORS
+const corsOptions = {
+  origin: 'http://shoppr-web-alb-1637775532.us-east-1.elb.amazonaws.com',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// Agregar encabezados CORS a todas las respuestas
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://shoppr-web-alb-1637775532.us-east-1.elb.amazonaws.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 // Rutas
 app.use("/api/motorcycles", crudRoutes);
 
 // Puerto
-const port = 4000
+const port = 4000;
 app.listen(port, () => console.log(`Server iniciado en el puerto: ${port}...`));
