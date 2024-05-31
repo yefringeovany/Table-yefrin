d75223178222a47c0e9f929101a0879e04e6e58b
yefringeovany <yperezm18@miumg.edu.gt>
2024-05-31 16:40:38 -0600

    Cambios archivos

--- server/index.js
+++ server/index.js
@@ -6,35 +6,23 @@ const crudRoutes = require("./routes/motorcycleRoutes");
 
 const app = express();
 
+
 // Conexión a la base de datos
 MongoDBConnection();
 
-// Middleware
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Configuración de CORS
-const corsOptions = {
-  origin: 'http://shoppr-web-alb-1637775532.us-east-1.elb.amazonaws.com',
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-// Agregar encabezados CORS a todas las respuestas
+app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://shoppr-web-alb-1637775532.us-east-1.elb.amazonaws.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+	res.locals.path = req.path;
+	next();
 });
+
 
 // Rutas
 app.use("/api/motorcycles", crudRoutes);
 
 // Puerto
-const port = 4000;
+const port = 4000
 app.listen(port, () => console.log(`Server iniciado en el puerto: ${port}...`));
--- server/models/motorcycleModel.js
+++ server/models/motorcycleModel.js
@@ -3,27 +3,22 @@ const mongoose = require("mongoose");
 const crudSchema = new mongoose.Schema({
 	marca: {
         type: String,
-        required: [true, "La marca es obligatoria"],
+       
     },
     modelo: {
         type: String,
-        required: [true, "El modelo es obligatorio"],
     },
     año: {
         type: Number,
-        required: [true, "El año es obligatorio"]
     },
     precio: {
         type: Number,
-        required: [true, "El precio es obligatorio"]
     },
     color: {
         type: String,
-        required: [true, "El color es obligatorio"],
     },
     kilometraje: {
         type: Number,
-        required: [true, "El kilometraje es obligatorio"]
     },
 });
 
